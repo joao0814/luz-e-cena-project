b8d88402f0fdc10356e96515c16164ba42afe034
joao0814 <joaopmfranca@gmail.com>
2025-07-20 12:55:15 -0300

    feat: create filter hook

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -1,23 +1,38 @@
-import useFetchMovies from "../../hooks/useFetchMovies.ts";
-import Button from "../Button/index.tsx";
-import Fieldset from "../Fieldset";
-import InputText from "../InputText.tsx";
-import MovieList from "../MovieList/index.tsx";
+import React from "react";
 import styles from "./MovieSection.module.css";
+import Fieldset from "../Fieldset";
+import Button from "../Button";
 import { FaSearch } from "react-icons/fa";
+import MovieList from "../MovieList";
+import useFetchMovies from "../../hooks/useFetchMovies";
+import useFilterMovies from "../../hooks/useFilterMovies";
+import InputText from "../InputText.tsx";
 
 const MovieSection = () => {
-  const { movies, loading, error } = useFetchMovies();
+  const { movies, error, loading } = useFetchMovies();
+  const { searchTerm, handleSearch, setSearchTerm, filteredMovies } =
+    useFilterMovies(movies);
+
   return (
-    <main className={styles.container}>
-      <Fieldset variant="secondary">
-        <InputText placeholder="Buscar filmes..." />
-        <Button variant="icon">
-          <FaSearch />
-        </Button>
-      </Fieldset>
-      <h1 className={styles.titulo}>Em cartaz</h1>
-      <MovieList movies={movies} />
+    <main>
+      <section className={styles.container}>
+        <Fieldset variant="secondary">
+          <InputText
+            value={searchTerm}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchTerm(event.target.value)
+            }
+            placeholder="Buscar filmes..."
+          />
+          <Button variant="icon" onClick={handleSearch}>
+            <FaSearch />
+          </Button>
+        </Fieldset>
+        <h1 className={styles.titulo}>Em cartaz</h1>
+        {loading && <p>Carregando filmes...</p>}
+        {error && <p className={styles.error}>{error}</p>}
+        <MovieList movies={filteredMovies} />
+      </section>
     </main>
   );
 };
